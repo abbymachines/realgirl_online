ec8661b57993a8ab0d342a9fe3d32f9943aa408e
Abby Castillo <abby.machines@gmail.com>
2025-01-03 15:04:23 -0800

    rough non-functional implementation of routes + W3schools tutorial on how to set up routes

--- src/App.js
+++ src/App.js
@@ -4,47 +4,66 @@ import FrontCover from './FrontCover.js'
 
 import db from './TableOfContents.js';
 
-const routes = [
-  {
-    path: "/about",
-    component: <AboutPage />,
-  },
-  {
-    path: "/issue/:issueId",
-    component: <RealGirlIssue />
-  },
-  {
-    path: "/issue/:issueId/post/:postId",
-    component: <RealGirlPost />
-  },
-];
+// const routes = [
+//   {
+//     path: "/about",
+//     component: <AboutPage />,
+//   },
+//   {
+//     path: "/issue/:issueId",
+//     component: <Issue />
+//   },
+//   {
+//     path: "/issue/:issueId/post/:postId",
+//     component: <Post />
+//   },
+// ];
 
-const { issueId, postId } = urlParams;
-function getPost() {
-  // const uri = db[issueId][postId];
-  // const data = makeGetRequest(uri);
-  const uri = db[]
-  return data;
-}
+// const { issueId, postId } = urlParams;
+// function getPost() {
+//   // const uri = db[issueId][postId];
+//   // const data = makeGetRequest(uri);
+//   const uri = db[]
+//   return data;
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="Title">
-          realgirl.online
-        </p>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p className="Title">
+    //       realgirl.online
+    //     </p>
 
-        <img src={logo} className="App-logo" alt="logo" />
+    //     <img src={logo} className="App-logo" alt="logo" />
 
-        <div className="Front-cover">
-          <FrontCover />
-        </div>
+    //     <div className="Front-cover">
+    //       <FrontCover />
+    //     </div>
 
         
-      </header>
-    </div>
+    //   </header>
+    // </div>
+
+    // For more info about how to configure routes, visit
+    // https://www.w3schools.com/react/react_router.asp
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="issue" element={<Issue />} />
+          <Route path="post" element={<Post />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
+
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 export default App;
