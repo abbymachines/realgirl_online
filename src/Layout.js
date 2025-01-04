593e24df57e8416b1cd22891407e37fbb8bebb18
abbymachines <abby.machines@gmail.com>
2025-01-04 13:04:51 -0800

    add home layout

--- src/Layout.js
+++ src/Layout.js
@@ -1,5 +1,28 @@
-export default function Layout() {
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
   return (
-    <div></div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/issue">Issue</Link>
+          </li>
+          <li>
+            <Link to="/post">Post</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
   )
-}
+};
+
+export default Layout;
