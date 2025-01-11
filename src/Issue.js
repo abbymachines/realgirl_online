c353aca69345c4f355d474f213ac84e4fa885caa
abbymachines <abby.machines@gmail.com>
2025-01-11 14:38:14 -0800

    remove commented-out import

--- src/Issue.js
+++ src/Issue.js
@@ -1,6 +1,5 @@
 import { Link, useParams } from "react-router-dom";
 import FrontCover from "./FrontCover";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "react-router-dom";
 import { render } from "@testing-library/react";
 
