56a52439ee51495c07899c7156c285dcc79c159f
Abby Castillo <abby.machines@gmail.com>
2025-01-16 17:37:10 -0800

    successfully make API call within post resource

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { useState } from 'react';
 
 import db from "./TableOfContents";
 
@@ -6,12 +7,26 @@ import './Post.css';
 
 export default function Post() {
   const { issueId, postId } = useParams();
+  const [data, setData] = useState(null);
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', `${db["issues"][issueId]["posts"][postId]["URL"]}`);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      setData(JSON.parse(xhr.responseText));
+    }
+  }
+  xhr.send();
+
 
   return (
     <div className="Post">
       <p>{db["issues"][issueId]["posts"][postId]["title"]}</p>
 
       <p>{db["issues"][issueId]["posts"][postId]["URL"]}</p>
+
+      {data ? <div>{JSON.stringify(data[0].content.rendered)}</div> : <div>Loading...</div>}
     </div>
   )
 }
