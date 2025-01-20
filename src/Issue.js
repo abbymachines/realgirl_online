9fa1fbdfa93f4413dfed5ec13ec0bd76b8670fed
abbymachines <abby.machines@gmail.com>
2025-01-19 16:59:34 -0800

    create empty files for pages

--- src/Issue.js
+++ src/Issue.js
@@ -2,6 +2,7 @@ import { Link, useParams } from "react-router-dom";
 import FrontCover from "./FrontCover";
 import "react-router-dom";
 import { render } from "@testing-library/react";
+import './Issue.css';
 
 export default function Issue(props) {
   const latestIssue = props.latestIssue;
@@ -16,13 +17,13 @@ export default function Issue(props) {
     if(issueIdInt <= 1) {
       return <span></span>
     } else {
-      return <span>⬅⬅⬅ <Link to={previousIssueURL}>PREVIOUS ISSUE</Link> |</span>
+      return <span><Link to={previousIssueURL}>⬅⬅⬅ PREVIOUS ISSUE</Link></span>
     }
   }
 
   function renderNextLink() {
     if(issueIdInt < latestIssue) {
-      return <span>| <Link to={nextIssueURL}>NEXT ISSUE</Link> ⮕⮕⮕</span>
+      return <span><Link to={nextIssueURL}>NEXT ISSUE ⮕⮕⮕</Link></span>
     } else {
       return <span></span>
     }
@@ -32,7 +33,7 @@ export default function Issue(props) {
     <div>
       <FrontCover issue={issueId} />
 
-      <center>
+      <center className="Issue-nav">
         {renderPreviousLink()} {renderNextLink()}
       </center>
     </div>
