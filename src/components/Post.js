e4035f38aef63f8e3efadc0876bf1c564fe4d643
abbymachines <abby.machines@gmail.com>
2025-01-25 13:52:26 -0800

    implement useEffect hook

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,36 +1,55 @@
 import { useParams } from "react-router-dom";
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 import PostNav from "./PostNav";
-
 import db from "../TableOfContents";
-
 import './Post.css';
 
 export default function Post(props) {
   const { issueId, postId } = useParams();
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
   const latestIssueId = props.latestIssueId;
 
-  const xhr = new XMLHttpRequest();
+  useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    
+    xhr.open('GET', `${db["issues"][issueId]["posts"][postId]["URL"]}`);
+    
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        setData(JSON.parse(xhr.responseText));
+      } else {
+        setError('Failed to fetch data');
+      }
+    };
 
-  xhr.open('GET', `${db["issues"][issueId]["posts"][postId]["URL"]}`);
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      setData(JSON.parse(xhr.responseText));
-    }
+    xhr.onerror = function() {
+      setError('Network error occurred');
+    };
+
+    xhr.send();
+
+    // Cleanup function
+    return () => {
+      xhr.abort(); // Abort the request if component unmounts
+    };
+  }, [issueId, postId]); // Dependencies array with relevant values
+
+  if (error) {
+    return <div>Error: {error}</div>;
   }
-  xhr.send();
-
 
   return (
     <div>
       <div className="Post">
         <p>{issueId}.{postId}<br />{db["issues"][issueId]["posts"][postId]["title"]}</p>
-
-        {data ? <div dangerouslySetInnerHTML={{__html: data[0].content.rendered}} /> : <div>Loading...</div>}
+        {data ? (
+          <div dangerouslySetInnerHTML={{__html: data[0].content.rendered}} />
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
       <PostNav issueId={issueId} latestIssueId={latestIssueId} postId={postId} />
     </div>
-  )
+  );
 }
