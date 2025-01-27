0947ec32742f2f8872b2514c0788b2fd264a5e03
abbymachines <abby.machines@gmail.com>
2025-01-26 19:21:41 -0800

    flesh out about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,14 +3,26 @@ import FrontCover from "../components/FrontCover";
 export default function AboutPage() {
   return (
     <div className="Front-cover">
-      <p>
+      <h1>
         realgirl.online is a webzine by Abby Castillo.
+      </h1>
+      <p>
+        this webzine is an experiment in getting lost and finding myself again. it is an adaptation of my print zine series, <i>realgirl1994</i>, which began as part of a 2020 residency at the Independent Publishing Resource Center in Portland, OR. 
+      </p>
+      <h2>
+        external links:
+      </h2>
+      <p>
+        <a href="https://abbymachines.gumroad.com/">online store</a>
       </p>
       <p>
-        <a href="https://patreon.com/abbymachines">Patreon</a>
+        <a href="https://patreon.com/abbymachines">patreon</a>
       </p>
       <p>
-        <a href="https://abbymachines.art">abbymachines.art</a>
+        <a href="https://abbymachines.art">artist website</a>
+      </p>
+      <p>
+        <a href="https://abbycastillo.dev">developer website</a>
       </p>
 
     </div>
