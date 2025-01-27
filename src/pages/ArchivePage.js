4e202b44c8a140dfec6a1f043a97c18f48330d20
abbymachines <abby.machines@gmail.com>
2025-01-26 19:00:04 -0800

    style archive links

--- src/pages/ArchivePage.js
+++ src/pages/ArchivePage.js
@@ -1,18 +1,7 @@
 import { render } from '@testing-library/react';
 import db from '../TableOfContents.js';
 import { Link } from 'react-router-dom';
-
-// let issues = {};
-// issueNames = 
-// let issueKeys = Object.keys(db['issues']);
-// let issuesLength = issueKeys.length + 1;
-
-// for (let i = 1; i < issuesLength; i++) {
-//   let issueName = db['issues'][i.toString()]['title'];
-//   let issueURL = `/issue/${i}`
-//   issues.push(issueName);
-//   issues[issueName] = issueURL
-// }
+import './ArchivePage.css';
 
 let issueNames = [];
 let issuesLength = Object.keys(db['issues']).length + 1;
@@ -33,7 +22,9 @@ function ArchivePage() {
           <p>
             {console.log(db['issues'][index + 1]['cover'])}
             
-            <Link to={`/issue/${index + 1}`}>{index + 1}. {title} <img src={renderCoverImage(index)}></img></Link>
+            <center>
+              <Link to={`/issue/${index + 1}`}>{index + 1}. {title} <br></br> <img className='Archive-item-image' src={renderCoverImage(index)}></img></Link>
+            </center>
           </p>)}
     </div>
   )
